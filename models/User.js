112dbdfa0b67bd0e652a7f8e64855ed325fe935f
BG00924 <BG00924@users.noreply.github.com>
2020-09-17 20:52:47 -0500

    lesson 2 complete

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const sequelize = require('../config/connection')
 const bycrypt = require('bcrypt')
 
 //create our user model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bycrypt.compareSync(loginPw, this.password)
+    }
+}
 
 //define table columns and configuration
 User.init(
